e2b6c7b57315fee441902922501b1e4738664ba8
ckzpedro <peucampelolemos@gmail.com>
2024-08-15 16:16:44 -0300

    mailto feature

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 function Rolagem() {
     function rolar(event) {
-        event.preventDefault(); 
-        var rol = event.target.getAttribute('href'); 
+        event.preventDefault();
+        var rol = event.target.getAttribute('href');
         var targetElement = document.querySelector(rol);
         if (targetElement)
             targetElement.scrollIntoView({ behavior: 'smooth' });
     }
 
     function inicia() {
-        document.querySelectorAll('nav.menu-desktop a').forEach(function(link) {
+        document.querySelectorAll('nav.menu-desktop a').forEach(function (link) {
             link.addEventListener('click', rolar);
         });
     }
-
     window.addEventListener('load', inicia);
 }
 
